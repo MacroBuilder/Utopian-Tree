be131943577802bf5c7fb735007c60a1c4334cc8
MacroBuilder <thomas.n.cool@gmail.com>
2015-06-08 12:57:58 -0700

    Create code.py

---
+++ code.py
@@ -0,0 +1,20 @@
+def utopian_tree(cycles):
+    height = 1
+    if cycles == 0:
+        return height
+    else:
+        switch = False
+        for i in range(0,cycles):
+            if switch == False:
+                height *= 2
+                switch = True
+            elif switch == True:
+                height += 1
+                switch = False
+        return height
+
+test_cases = int(raw_input())
+
+for _ in range(test_cases):
+    c = int(raw_input().strip())
+    print Utopian_Tree(c)
